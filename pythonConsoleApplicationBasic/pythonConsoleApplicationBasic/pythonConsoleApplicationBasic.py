b6803ab3b04638a9d99c6574b0ee1e69acbb710f
bronson <bronson@>
2018-03-04 14:44:31 +1300

    Added try exception

--- pythonConsoleApplicationBasic/pythonConsoleApplicationBasic/pythonConsoleApplicationBasic.py
+++ pythonConsoleApplicationBasic/pythonConsoleApplicationBasic/pythonConsoleApplicationBasic.py
@@ -4,19 +4,24 @@ print("Thank you for viewing my code!\nThis application is to show off some basi
 
 def optionMenuFunct(): #Create a function to be called for the option menu
 
-    print("Choose an option:\n%s\n%s\n" % ("1, The very basic calculator", "2, NotSureYet"))#Give the user some options to choose from
-    option = int(input("Please enter a number: ")) #Get the input from the user and make it an integer
 
 
-    if option == 1: #If the user presses 1 go to the calculator function
-        print("hi")
+    try:
+        print("Choose an option:\n%s\n%s\n" % ("1, The very basic calculator", "2, NotSureYet"))#Give the user some options to choose from
+        option = int(input("Please enter a number: ")) #Get the input from the user and make it an integer
 
-    elif option == 5: #Clear the Console and reload the option menu
+        if option == 1: #If the user presses 1 go to the calculator function
+            print("hi")
 
-        print("\n" * 50) #Apparently this is the only way without calling os commands
-        optionMenuFunct()
+        elif option == 5: #Clear the Console and reload the option menu
 
-    else: #Error catch, Just incase anyone presses anything thats not listed or accepted.
-        print("Please make sure you type in another between 1 and x")
+             print("\n" * 50) #Apparently this is the only way without calling os commands
+             optionMenuFunct()
+
+        else: #Error catch, Just incase anyone presses anything thats not listed or accepted.
+             print("Please make sure you type in another between 1 and x")
+             optionMenuFunct()
+    except:
+        print("Please make sure you entered a number between 1 and x")
 
 optionMenuFunct() #call the optionMenuFunct to bring up the option menu
